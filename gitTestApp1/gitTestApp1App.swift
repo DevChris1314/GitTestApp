2e933ff2e4a34ade8bb42c6975c5dc0900a9ec3e
Cristian <devchris1314@gmail.com>
2025-01-30 13:07:09 -0500

    Intial Commit

---
+++ gitTestApp1/gitTestApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestApp1App.swift
+//  gitTestApp1
+//
+//  Created by Cristian Molina  on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct gitTestApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
